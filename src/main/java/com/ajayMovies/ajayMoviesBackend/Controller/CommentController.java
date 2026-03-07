e5960d447dcff2e9ed1cc9f9d02f57400a211496
ankitdoi--coder <ankitdoi82@gmail.com>
2026-03-08 00:05:55 +0530

    integration with Frontend

--- src/main/java/com/ajayMovies/ajayMoviesBackend/Controller/CommentController.java
+++ src/main/java/com/ajayMovies/ajayMoviesBackend/Controller/CommentController.java
@@ -7,7 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,7 +25,7 @@ public class CommentController {
 
     //api to add comment
     @PostMapping("/add-comment")
-    public ResponseEntity<Comment> addComment(@RequestBody CommentDTO commentDTO){
+    public ResponseEntity<Comment> addComment(@RequestPart("comment") CommentDTO commentDTO){
         return ResponseEntity.ok(commentService.addComment(commentDTO));
     }
 
